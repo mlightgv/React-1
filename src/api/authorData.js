e1da5c5655804bf112999b73d69b9a9be45db907
Mary Gomez Villarreal <mlight@192-168-1-33.tpgi.com.au>
2016-10-16 15:05:56 +1100

    add first project React

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+  authors:
+  [
+    {
+      id: 'cory-house',
+      firstName: 'cory',
+      lastName: 'House'
+    },
+    {
+      id: 'scott-allen',
+      firstName: 'Scott',
+      lastName: 'Allen'
+    },
+    {
+      id: 'dan-wahlin',
+      firstName: 'Dan',
+      lastName: 'Wahlin'
+    }
+  ]
+};
