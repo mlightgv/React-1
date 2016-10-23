555d6a2818c3313400fcfd5a72eab50eb0f11a08
Mary Gomez <mary.gomez.america@gmail.com>
2016-10-23 21:14:05 +1100

    fix errors

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -50726,33 +50726,33 @@ module.exports = routes;
 
 var Dispatcher = require('../dispatcher/appDispatcher');
 var ActionTypes = require('../constants/actionTypes');
-var EventEmmitter = require('events').EventEmmitter;
+var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
 var _ = require('lodash');
 var CHANGE_EVENT = 'change';
 
 var _authors = [];
 
-var AuthorStore = assign({}, EventEmmitter.prototype, {
-  addChangeListener: function(callback) {
-    this.on(CHANGE_EVENT, callback);
-  },
+var AuthorStore = assign({}, EventEmitter.prototype, {
+	addChangeListener: function(callback) {
+		this.on(CHANGE_EVENT, callback);
+	},
 
-  removeChangeListener: function(callback) {
-    this.removeListener(CHANGE_EVENT, callback);
-  },
+	removeChangeListener: function(callback) {
+		this.removeListener(CHANGE_EVENT, callback);
+	},
 
-  emitChange: function() {
-    this.emit(CHANGE_EVENT);
-  },
+	emitChange: function() {
+		this.emit(CHANGE_EVENT);
+	},
 
-  getAllAuthors: function() {
-    return _authors;
-  },
+	getAllAuthors: function() {
+		return _authors;
+	},
 
-  getAuthorById: function(id) {
-    return _.find(_authors, {id: id});
-  }
+	getAuthorById: function(id) {
+		return _.find(_authors, {id: id});
+	}
 });
 
 Dispatcher.register(function(action) {
