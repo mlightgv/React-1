09cc753867d49236f1fc68ce0faf7a4f9a50eb13
Mary Gomez <mary.gomez.america@gmail.com>
2016-10-30 13:00:19 +1100

    add new routes

--- src/routes.js
+++ src/routes.js
@@ -11,11 +11,18 @@ var Redirect = Router.Redirect;
 var routes = (
   <Route name="app" path="/" handler={require('./components/app')}>
     <DefaultRoute handler={require('./components/homePage')} />
+
     <Route name="authors" handler={require('./components/authors/authorPage')} />
     <Route name="addAuthor" path="author" handler={require('./components/authors/manageAuthorPage')} />
     <Route name="manageAuthor" path="author/:id" handler={require('./components/authors/manageAuthorPage')} />
-    <Route name="about" handler={require('./components/about/aboutPage')} />
+
+    <Route name="courses" handler={require('./components/courses/coursePage')} />
+    <Route name="addCourse" path="course" handler={require('./components/courses/manageCoursePage')} />
+    <Route name="manageCourse" path="course/:id" handler={require('./components/courses/manageCoursePage')} />
+
     <NotFoundRoute handler={require('./components/notFoundPage')} />
+
+    <Route name="about" handler={require('./components/about/aboutPage')} />
     <Redirect from="about-us" to="about" />
     <Redirect from="awthurs" to="authors" />
     <Redirect from="about/*" to="about" />
